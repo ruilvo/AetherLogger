8c356768d6d436efdff34a602c4ad753f1741eba
Rui Oliveira <ruimail24@gmail.com>
2023-12-03 13:18:00 +0000

    Move the Models into their own project

---
+++ AetherLogger.Models/Location.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+
+namespace AetherLogger.Models;
+
+public struct DMS
+{
+    public int Degrees { get; set; }
+    public int Minutes { get; set; }
+    public double Seconds { get; set; }
+
+    public readonly double DecimalDegrees
+    {
+        get
+        {
+            return Degrees + Minutes / 60.0 + Seconds / 3600.0;
+        }
+    }
+
+    public DMS(double value)
+    {
+        Degrees = (int)value;
+        Minutes = (int)((value - Degrees) * 60);
+        Seconds = (double)((value - Degrees - Minutes / 60.0) * 3600.0);
+    }
+}
+
