d7dadba5672453b4102673716383ed9898a56bf6
Rui Oliveira <ruimail24@gmail.com>
2023-12-03 15:24:53 +0000

    Allow converting to and from QTH locators

---
+++ AetherLogger.Models/DegMinSec.cs
@@ -0,0 +1,20 @@
+﻿namespace AetherLogger.Models;
+
+public struct DegMinSec
+{
+    public double Degrees { get; set; }
+    public double Minutes { get; set; }
+    public double Seconds { get; set; }
+
+    public DegMinSec(double value)
+    {
+        Degrees = Math.Floor(value);
+        Minutes = Math.Floor((value - Degrees) * 60);
+        Seconds = (value - Degrees - Minutes / 60.0) * 3600.0;
+    }
+
+    public readonly double ToDecimalDegrees()
+    {
+        return Degrees + Minutes / 60.0 + Seconds / 3600.0;
+    }
+}
