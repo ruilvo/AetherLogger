5b93f6c8c6f892502daef090553dc38111c71fac
Rui Oliveira <ruimail24@gmail.com>
2023-12-06 11:08:44 +0000

    Add ISmartEnum

---
+++ AetherLogger.Models/ISmartEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace AetherLogger.Models;
+
+public interface ISmartEnum : Ardalis.SmartEnum.ISmartEnum
+{
+    // Ardalis.SmartEnum.ISmartEnum is defined empty,
+    // but it should have a Name and Value property.
+    string Name { get; }
+    int Value { get; }
+}
