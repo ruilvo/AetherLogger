aab1c8b3ddd147e35e40346e63b606f05c77aa36
Rui Oliveira <ruimail24@gmail.com>
2024-06-15 10:21:59 +0100

    Add a tests project for the base utilities

---
+++ AetherLogger.Base.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: 2024 Rui Oliveira <ruimail24@gmail.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+namespace AetherLogger.Base.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
