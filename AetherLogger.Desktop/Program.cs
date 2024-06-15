d841b450ee1d80462207abf27d200343fa6599cf
Rui Oliveira <ruimail24@gmail.com>
2024-06-15 10:21:08 +0100

    Add Avalonia desktop project

---
+++ AetherLogger.Desktop/Program.cs
@@ -0,0 +1,27 @@
+﻿// SPDX-FileCopyrightText: 2024 Rui Oliveira <ruimail24@gmail.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+
+using Avalonia;
+
+namespace AetherLogger.Desktop;
+
+class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
+
+}
