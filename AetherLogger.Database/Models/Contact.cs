2a9d70edc42de9ce4ddc1a1f4c1e329ba4e11218
Rui Oliveira <ruimail24@gmail.com>
2024-08-03 17:24:04 +0100

    Add basic info for a QSO

---
+++ AetherLogger.Database/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿// This file is part of AetherLogger
+// SPDX-FileCopyrightText: 2024 Rui Oliveira <ruimail24@gmail.com>
+// SPDX-License-Identifier: Apache-2.0
+
+namespace AetherLogger.Database.Models;
+
+public class Contact
+{
+    public Operator? Operator { get; set; }
+    public DateTime TimeOn { get; set; }
+
+}
+
