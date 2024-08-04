8a53fcfcfa53e25906520317c6153ec21394e53b
Rui Oliveira <ruimail24@gmail.com>
2024-08-04 10:42:12 +0100

    Update Contact class to include required fields and submode validation

--- AetherLogger.Database/Models/Contact.cs
+++ AetherLogger.Database/Models/Contact.cs
@@ -2,12 +2,32 @@
 // SPDX-FileCopyrightText: 2024 Rui Oliveira <ruimail24@gmail.com>
 // SPDX-License-Identifier: Apache-2.0
 
+using System.Linq;
+
 namespace AetherLogger.Database.Models;
 
 public class Contact
 {
-    public Operator? Operator { get; set; }
-    public DateTime TimeOn { get; set; }
+    public required string Callsign { get; set; }
+    public required DateTime TimeOn { get; set; }
 
+    public required Mode Mode { get; set; }
+
+    private IMode? _submode;
+    public IMode? Submode
+    {
+        get => _submode;
+        set
+        {
+            if (Mode.Submodes != null && Mode.Submodes.Contains(value))
+            {
+                _submode = value;
+            }
+            else
+            {
+                throw new ArgumentException("Submode not valid for mode", nameof(value));
+            }
+        }
+    }
 }
 
