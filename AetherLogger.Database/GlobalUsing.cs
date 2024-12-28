c7ca61a9aba3a2a931ace8f217704cb0bc3cd7f2
Rui Oliveira <ruimail24@gmail.com>
2024-12-28 15:32:49 +0000

    Add GlobalUsing.cs for global using directives in AetherLogger.Database

---
+++ AetherLogger.Database/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿// This file is part of AetherLogger
+// SPDX-FileCopyrightText: 2024 Rui Oliveira <ruimail24@gmail.com>
+// SPDX-License-Identifier: Apache-2.0
+
+global using System;
+global using System.Collections.Generic;
