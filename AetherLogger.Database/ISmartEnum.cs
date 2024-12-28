48a909ee37b6a0a9e49edb9ce9a4c59f9d841ef4
Rui Oliveira <ruimail24@gmail.com>
2024-12-28 15:32:55 +0000

    Add ISmartEnum interface to AetherLogger.Database for enhanced enum functionality

---
+++ AetherLogger.Database/ISmartEnum.cs
@@ -0,0 +1,14 @@
+﻿// This file is part of AetherLogger
+// SPDX-FileCopyrightText: 2024 Rui Oliveira <ruimail24@gmail.com>
+// SPDX-License-Identifier: Apache-2.0
+
+
+namespace AetherLogger.Database;
+
+public interface ISmartEnum : Ardalis.SmartEnum.ISmartEnum
+{
+    // Ardalis.SmartEnum.ISmartEnum is defined empty,
+    // but it should have a Name and Value property.
+    string Name { get; }
+    int Value { get; }
+}
